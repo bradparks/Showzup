e4aeb5a86548c2127e292dbf86b32b33a1b7a4c6
Mathieu Frenette <mfrenette@lvlstudio.com>
2017-02-06 17:08:26 -0500

    Added filtering extensions methods.

--- Assets/Showzup/Interfaces/Phases/IPhasedPresenterExtensions.cs
+++ Assets/Showzup/Interfaces/Phases/IPhasedPresenterExtensions.cs
@@ -8,8 +8,26 @@ namespace Silphid.Showzup
         public static IObservable<TPhase> OfPhase<TPhase>(this IObservable<IPhase> This) where TPhase : IPhase =>
             This.OfType<IPhase, TPhase>();
 
+        public static IObservable<IPhase> From<TView>(this IObservable<IPhase> This) where TView : IView =>
+            This.Where(x => x.SourceViewType.IsAssignableTo<TView>());
+
+        public static IObservable<IPhase> To<TView>(this IObservable<IPhase> This) where TView : IView =>
+            This.Where(x => x.TargetViewType.IsAssignableTo<TView>());
+
+        public static IObservable<IPhase> Between<TSource, TTarget>(this IObservable<IPhase> This) where TSource : IView where TTarget : IView =>
+            This.From<TSource>().To<TTarget>();
+
         public static IObservable<CompletedPhase> OfPhase<TPhase>(this IObservable<CompletedPhase> This) where TPhase : IPhase =>
             This.Where(x => x.PhaseType.IsAssignableTo<TPhase>());
+
+        public static IObservable<CompletedPhase> From<TView>(this IObservable<CompletedPhase> This) where TView : IView =>
+            This.Where(x => x.SourceViewType.IsAssignableTo<TView>());
+
+        public static IObservable<CompletedPhase> To<TView>(this IObservable<CompletedPhase> This) where TView : IView =>
+            This.Where(x => x.TargetViewType.IsAssignableTo<TView>());
+
+        public static IObservable<CompletedPhase> Between<TSource, TTarget>(this IObservable<CompletedPhase> This) where TSource : IView where TTarget : IView =>
+            This.From<TSource>().To<TTarget>();
 
         public static IObservable<bool> IsPresenting(this IPhasedPresenter This) =>
             This.PresentationStarting
