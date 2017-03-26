9c004de5d3cbc34d4f2790c79bba814ea5021551
Mathieu Frenette <mfrenette@lvlstudio.com>
2017-03-26 16:40:13 -0400

    Added Pre/Post/Show/Hide support in ItemControl.

--- Assets/Showzup/Controls/ItemControl.cs
+++ Assets/Showzup/Controls/ItemControl.cs
@@ -13,11 +13,48 @@ namespace Silphid.Showzup
             if (Container == null)
                 throw new InvalidOperationException($"Must specify ContentContainer property of ContentControl {gameObject}");
 
-            ReplaceView(Container, view);
-            view.IsActive = true;
-            _view.Value = view;
+            var sourceView = _view.Value;
+            var targetView = view;
+
+            PreHide(sourceView, options);
+            PreShow(targetView, options);
+
+            ReplaceView(Container, targetView);
+            targetView.IsActive = true;
+            _view.Value = targetView;
+
+            PostHide(sourceView, options);
+            PostShow(targetView, options);
 
             return Observable.ReturnUnit();
+        }
+
+        private void PreHide(IView view, Options options)
+        {
+            var preHide = view as IPreHide;
+            if (preHide != null)
+                preHide.OnPreHide(options);
+        }
+
+        private void PreShow(IView view, Options options)
+        {
+            var preShow = view as IPreShow;
+            if (preShow != null)
+                preShow.OnPreShow(options);
+        }
+
+        private void PostShow(IView view, Options options)
+        {
+            var postShow = view as IPostShow;
+            if (postShow != null)
+                postShow.OnPostShow(options);
+        }
+
+        private void PostHide(IView view, Options options)
+        {
+            var postHide = view as IPostHide;
+            if (postHide != null)
+                postHide.OnPostHide(options);
         }
     }
 }
